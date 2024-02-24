46f3b30439b106c8bfadb6da5752fc1c9e416bec
MNAth_ <neodarksoulink@gmail.com>
2024-02-24 20:19:42 +0300

    refactor: download template as tar file

--- cli.js
+++ cli.js
@@ -6,20 +6,32 @@ const { execSync } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
-
-/** Git repo source source. */
-const source = "https://github.com/cypher-space/V0.9-Beta.git";
+const tar = require("tar");
+const { promisify } = require("util");
+const { pipeline } = require("stream");
+const pipelineAsync = promisify(pipeline);
 
 const init = async () => {
 	try {
-		console.log("📌 Cypher.space Template initialization...");
-		/** @type {{ name: string; blog: boolean; store: boolean; lnurl: string; btcadress: string; finance: boolean; albytoken: boolean, albytoken?: string }} */
+		// DATA
+		const owner = "cypher-space";
+		const repoName = "V0.9-Beta";
+
+		/** @type {string | undefined} */
+		let projectDir;
+		/** @type {{[name:string]: number | string | {[name:string]: any }} | undefined} */
+		let projectPackage;
+		/** @type {{[name:string]: number | string | {[name:string]: any }} | undefined} */
+		let projectSetupConfig;
+
+		console.log(`📌 ${owner} Template initialization...`);
+		/** @type {{ name?: string; blog: boolean; store: boolean; lnurl?: string; btcadress?: string; orderwebhook>: string; finance: boolean; albytoken?: string }} */
 		const answers = await inquirer.prompt([
 			{
 				type: "input",
 				name: "name",
 				message: "🛠  What is the name of your project: ",
-				default: "cypher.space",
+				default: owner,
 			},
 			{
 				type: "confirm",
@@ -37,13 +49,16 @@ const init = async () => {
 				type: "input",
 				name: "lnurl",
 				message: "🛠  Enter your Lightning LNUrl: ",
-				default: "",
 			},
 			{
 				type: "input",
 				name: "btcadress",
 				message: "🛠  Enter a receiving bitcoin segwit address for your store: ",
-				default: "",
+			},
+			{
+				type: "input",
+				name: "orderwebhook",
+				message: "🛠  Set your order webhook: ",
 			},
 			{
 				type: "confirm",
@@ -67,39 +82,70 @@ const init = async () => {
 					])
 				).albytoken ?? "";
 
+		projectDir = path.resolve(process.cwd(), answers.name);
+
 		try {
 			console.log(
 				"\x1b[36m%s\x1b[0m",
-				`🛠 Cloning 'cypher-space' template into ${answers.name}...`
+				`🛠 Fetching 'cypher-space' template into ${answers.name}...`
 			);
-			execSync(`git clone --depth 1 ${source} ${answers.name}`, {
-				stdio: "inherit",
+			const apiUrl = `https://api.github.com/repos/${owner}/${repoName}/tarball/main`;
+			const tmpDir = path.join(__dirname, ".cypher-space-tmp");
+			fs.mkdirSync(tmpDir, { recursive: true });
+			const tarPath = path.join(tmpDir, "downloaded_repo.tar");
+			const response = await fetch(apiUrl, {
+				headers: {
+					"User-Agent": "cypher-space",
+					Accept: "application/vnd.github.v3.raw",
+				},
 			});
 
-			fs.rmSync(path.join(__dirname, answers.name, ".git"), {
-				recursive: true,
+			if (!response.ok)
+				throw `🛑 Failed to fetch repository. Status code: ${response.status}`;
+
+			await pipelineAsync(response.body, fs.createWriteStream(tarPath));
+			await tar.extract({
+				file: tarPath,
+				cwd: tmpDir,
+				sync: true,
 			});
+			fs.unlinkSync(tarPath);
+
+			const extractedFolderName = fs.readdirSync(tmpDir)[0];
+			const newDestination = path.join(__dirname, answers.name);
+
+			fs.renameSync(path.join(tmpDir, extractedFolderName), newDestination);
+			fs.rmSync(tmpDir, { recursive: true });
 		} catch (_) {
-			throw "🛑 Unable to clone the template";
+			console.log(_);
+			throw typeof _ === "string" ? _ : "🛑 Unable to install the template";
+		}
+
+		try {
+			projectPackage = require(path.join(projectDir, "package.json"));
+			projectSetupConfig = require(path.join(projectDir, "config/setup.json"));
+
+			if (!projectDir || !projectPackage || !projectSetupConfig)
+				throw new Error();
+		} catch (_) {
+			throw "🛑 Unable to load metadata";
 		}
 
 		try {
 			console.log("\x1b[36m%s\x1b[0m", `🛠 Configuring template...`);
-			const projectDir = path.resolve(process.cwd(), answers.name);
-			const projectPackage = require(path.join(projectDir, "package.json"));
-			const projectSetupConfig = require(path.join(
-				projectDir,
-				"config/setup.json"
-			));
-
 			projectPackage.name = answers.name;
 
-			projectSetupConfig.name = answers.name;
-			projectSetupConfig.blog = answers.blog;
-			projectSetupConfig.shop = answers.shop;
-			projectSetupConfig.lnurl = answers.lnurl;
-			projectSetupConfig.btcadress = answers.btcadress;
-			projectSetupConfig.finance = answers.finance;
+			projectSetupConfig.name = answers.name ?? projectSetupConfig.name;
+			projectSetupConfig.textlogo = answers.name ?? projectSetupConfig.name;
+			projectSetupConfig.blog = answers.blog ?? projectSetupConfig.blog;
+			projectSetupConfig.shop = answers.shop ?? projectSetupConfig.shop;
+			projectSetupConfig.lnurl = answers.lnurl ?? projectSetupConfig.lnurl;
+			projectSetupConfig.btcadress =
+				answers.btcadress ?? projectSetupConfig.btcadress;
+			projectSetupConfig.btcadress =
+				answers.btcadress ?? projectSetupConfig.btcadress;
+			projectSetupConfig.finance =
+				answers.finance ?? projectSetupConfig.finance;
 			if (answers.albytoken) projectSetupConfig.albytoken = answers.albytoken;
 
 			fs.writeFileSync(
@@ -130,7 +176,7 @@ const init = async () => {
 		);
 		console.log(
 			"\x1b[32m%s\x1b[0m",
-			"📌 Don't worry, you can modify those settings any time in `projectName/config/setup.json`"
+			`📌 Don't worry, you can modify those settings at any times in '${answers.name}/config/setup.json'`
 		);
 	} catch (err) {
 		console.warn(
